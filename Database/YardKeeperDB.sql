0a0ceac30ac532f320765b72cb2a1bf69a676d7b
Chris Jobin-Bevans <122691993+Wookie3@users.noreply.github.com>
2023-04-14 21:54:28 -0400

    Update YardKeeperDB.sql

--- Database/YardKeeperDB.sql
+++ Database/YardKeeperDB.sql
@@ -1,93 +1,107 @@
+START TRANSACTION;
 DROP DATABASE IF EXISTS yard_keeper;
 CREATE DATABASE yard_keeper;
+USE yard_keeper;
 
-
+DROP TABLE IF EXISTS yard_keeper.staff;
 CREATE TABLE yard_keeper.staff(
 	staff_id INT PRIMARY KEY AUTO_INCREMENT, 
 	first_name VARCHAR(35) NOT NULL, 
 	last_name VARCHAR(35) NOT NULL, 
-	is_crew_lead BOOLEAN NOT NULL
+	is_crew_lead BOOLEAN DEFAULT 0,
+    is_admin BOOLEAN DEFAULT 0
 );
+
+INSERT INTO yard_keeper.staff(first_name, last_name, is_crew_lead, is_admin)
+VALUES
+	('Jon', 'Doe', '1', '0'),
+    ('Debbie', 'Downer', '0', '0'),
+    ('Jack', 'Nickles', '0', '0'),
+    ('Toby', 'Jorden', '0', '1')
+;
+DROP TABLE IF EXISTS yard_keeper.sign_out;
+CREATE TABLE yard_keeper.sign_out(
+	item_id INT PRIMARY KEY UNIQUE,
+    item_type VARCHAR(48) NOT NULL,
+	time_updated DATETIME,
+    is_out BOOLEAN DEFAULT 0,
+    staff_id INT DEFAULT NULL,
+    CONSTRAINT fk_sign_out_staffid
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
+);
+
+INSERT INTO yard_keeper.sign_out(item_id, item_type, time_updated)
+VALUES
+	(2010, 'truck', current_timestamp()),
+    (2020, 'truck', current_timestamp()),
+    (2030, 'truck', current_timestamp()),
+    (2040, 'tool_chest', current_timestamp()),
+    (2050, 'mower', current_timestamp())
+;
+
 
 
 CREATE TABLE yard_keeper.users(
 	user_id INT PRIMARY KEY AUTO_INCREMENT,
     user_name VARCHAR(36) UNIQUE,
-    user_password VARCHAR(18)
+    user_password VARCHAR(28)
 );
 
+INSERT INTO yard_keeper.users(user_name, user_password)
+	VALUES
+		('New City Mall', 'super_password'),
+        ('Building Inc.', 'pass_word'),
+        ('Greenbelt Properties', 'pa55w0rd')
+;
 
 CREATE TABLE yard_keeper.locations(
 	location_id INT PRIMARY KEY UNIQUE,
 	street_name VARCHAR(48) NOT NULL,
-	street_number VARCHAR(48) NOT NULL,
-	postal_code VARCHAR(6)
+	street_number VARCHAR(12) NOT NULL,
+	postal_code VARCHAR(6),
+    user_id INT,
+	CONSTRAINT fk_locations_user_id
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
+	ON UPDATE CASCADE
+    ON DELETE SET NULL
 );
 
 
+INSERT INTO yard_keeper.locations(location_id, street_name, street_number, postal_code, user_id)
+VALUES 
+	('101', 'Weston Rd.', '1800', 'M7J2E6', '001'),
+    ('102', 'King St. E', '201', 'M5E2X1', '002'),
+    ('103', 'King St. E', '800', 'M5E3P4', '002'),
+    ('104', 'Front St. W', '550', 'M6J2E4', '003'),
+    ('105', 'Queen St. W', '106', 'M7F1R6', '003')
+;
+
+
+DROP TABLE IF EXISTS yard_keeper.tasks;
 CREATE TABLE yard_keeper.tasks(
-task_id INT PRIMARY KEY AUTO_INCREMENT,
-task_info VARCHAR(255),
-is_complete BOOLEAN
-);
-
-
-ALTER TABLE users 
-	ADD COLUMN location_id INT,
-	ADD CONSTRAINT fk_users_location_id
+	task_id INT PRIMARY KEY AUTO_INCREMENT,
+	task_info VARCHAR(255),
+    time_added DATETIME DEFAULT CURRENT_TIMESTAMP,
+    time_complete DATETIME DEFAULT NULL,
+	is_complete BOOLEAN DEFAULT 0,
+    location_id INT,
+    CONSTRAINT fk_tasks_location_id 
 	FOREIGN KEY (location_id) REFERENCES locations(location_id)
 	ON UPDATE CASCADE
-	ON DELETE CASCADE;
-	
-ALTER TABLE tasks 
-	ADD COLUMN location_id INT, 
-    ADD CONSTRAINT fk_tasks_location_id 
-	FOREIGN KEY (location_id) REFERENCES locations(location_id)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE;
-
-
-ALTER TABLE locations 
-	ADD COLUMN task_id INT, 
-	ADD COLUMN user_id INT,
-    ADD CONSTRAINT fk_locations_task_id
-		FOREIGN KEY (task_id) REFERENCES tasks(task_id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-	ADD CONSTRAINT fk_locations_user_id
-		FOREIGN KEY (user_id) REFERENCES users(user_id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
-
-CREATE TABLE yard_keeper.active_tasks(
-task_id INT,
-is_complete BOOLEAN,
-location_id INT,
-CONSTRAINT fk_active_tasks_task_id
-	FOREIGN KEY (task_id) REFERENCES tasks(task_id)
-    ON UPDATE CASCADE,
-CONSTRAINT fk_active_tasks_is_complete
-	FOREIGN KEY (is_complete) REFERENCES tasks(is_complete)
-    ON UPDATE CASCADE,
-CONSTRAINT fk_active_tasks_location_id
-	FOREIGN KEY (location_id) REFERENCES locations(location_id)
-    ON UPDATE CASCADE
+    ON DELETE SET NULL
 );
 
 
--- SCRIPTS FOR TEST
-SELECT task_id, is_complete, staff_id 
-	FROM tasks AS t
-    JOIN staff AS s
-    ON s.staff_id = t.staff_id;
 
-SELECT * FROM tasks;
-SELECT * FROM locations;
--- update staff memeber
-UPDATE staff SET is_crew_lead = '1' WHERE staff_id = '4';
--- update a user to add a location
-UPDATE users SET location_id = '006' WHERE user_id ='2';
-SELECT * from users;
-SELECT * from staff;
-SELECT * from locations;
-SELECT * FROM staff WHERE is_crew_lead;
+INSERT INTO yard_keeper.tasks(task_info, is_complete, location_id)
+VALUES
+	('Mow the all the back lawn around the pool area', '0', 101),
+    ('Cut all hedges in the front facing area', '0', 101),
+    ('Mutch all front flower beds.', '0', 103),
+    ('Mow back lawn', '0', 102),
+    ('Multch front beds', '0', 103),
+    ('Multch back beds', '0', 103)
+;
+COMMIT;
